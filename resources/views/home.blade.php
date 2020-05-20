0788eb325bd677205f0e95aeec188daa08fcca5a
unknown <dracbolteon@gmail.com>
2020-05-20 00:49:50 -0500

    home nuevo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,2 +1,205 @@
 @extends('layouts.master')
 
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card mt-4">
+              <div class="card-body">
+                <div class="row">
+                  <div class="col-4 form-group">
+                      <label for="exampleInputEmail1">Name</label>
+                      <input type="text" class="form-control" placeholder="Carlos Cabrera">
+                  </div>
+                  <div class="col-4 form-group">
+                      <label for="exampleInputEmail1">Email </label>
+                      <input type="text" class="form-control" placeholder="jose@gmail.com">
+                  </div>
+                  <div class="col-4 form-group">
+                      <label for="exampleInputEmail1">Type</label>
+                      <select class="form-control">
+                              <option>Admin</option>
+                              <option>User</option>
+                              <option>Author</option>
+                      </select>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="row mt-2" >
+              <div class="col-md-12">
+                <div class="card">
+                  <div class="card-header">
+                    <h3 class="card-title">Users</h3>
+                    <div class="card-tools">
+                      <button class="btn  btn-outline-success" ><i class="fa fa-user-plus fa-fw"></i> Add User</button>
+                    </div>
+                  </div>
+                  <!-- /.card-header -->
+                  <div class="card-body table-responsive px-3">
+                      <form class="form-horizontal">
+                        <div class="form-group row">
+                          <div class="col-md-5 d-inline-block">
+                            <span>Mostrar</span> 
+                              <select class="custom-select select-numfilas ml-2 mr-2" style="width :75px;">
+                                  <option value="5">5</option>
+                                  <option value="5">5</option>
+                                  <option value="5">5</option>
+                                  <option value="5">5</option>
+                              </select>
+                            <span>filas</span>
+                          </div>
+                        </div>
+                      </form>
+                    <table class="table table-hover table-striped table-sm  text-nowrap">
+                      <thead>
+                        <tr>
+                          <th>ID</th>
+                          <th>Name</th>
+                          <th>Email</th>
+                          <th>Type</th>
+                          <th>Registered at</th>
+                          <th>Actions</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>1</td>
+                          <td>Admin admin</td>
+                          <td>admin@gmail.com</td>
+                          <td><span class="tag tag-danger">ADMIN</span></td>
+                          <td>25/12/2018</td>
+                          <td>
+                            <button class="btn btn-outline-primary btn-sm">
+                              <i class="fa fa-edit"></i>
+                            </button>
+                            <button class="btn btn-outline-danger btn-sm ml-1">
+                              <i class="fa fa-trash"></i>
+                            </button>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>1</td>
+                          <td>Admin admin</td>
+                          <td>admin@gmail.com</td>
+                          <td><span class="tag tag-danger">ADMIN</span></td>
+                          <td>25/12/2018</td>
+                          <td>
+                            <button class="btn btn-outline-primary btn-sm">
+                              <i class="fa fa-edit"></i>
+                            </button>
+                            <button class="btn btn-outline-danger btn-sm ml-1">
+                              <i class="fa fa-trash"></i>
+                            </button>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>1</td>
+                          <td>Admin admin</td>
+                          <td>admin@gmail.com</td>
+                          <td><span class="tag tag-danger">ADMIN</span></td>
+                          <td>25/12/2018</td>
+                          <td>
+                            <button class="btn btn-outline-primary btn-sm">
+                              <i class="fa fa-edit"></i>
+                            </button>
+                            <button class="btn btn-outline-danger btn-sm ml-1">
+                              <i class="fa fa-trash"></i>
+                            </button>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>1</td>
+                          <td>Admin admin</td>
+                          <td>admin@gmail.com</td>
+                          <td><span class="tag tag-danger">ADMIN</span></td>
+                          <td>25/12/2018</td>
+                          <td>
+                            <button class="btn btn-outline-primary btn-sm">
+                              <i class="fa fa-edit"></i>
+                            </button>
+                            <button class="btn btn-outline-danger btn-sm ml-1">
+                              <i class="fa fa-trash"></i>
+                            </button>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>1</td>
+                          <td>Admin admin</td>
+                          <td>admin@gmail.com</td>
+                          <td><span class="tag tag-danger">ADMIN</span></td>
+                          <td>25/12/2018</td>
+                          <td>
+                            <button class="btn btn-outline-primary btn-sm">
+                              <i class="fa fa-edit"></i>
+                            </button>
+                            <button class="btn btn-outline-danger btn-sm ml-1">
+                              <i class="fa fa-trash"></i>
+                            </button>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>1</td>
+                          <td>Admin admin</td>
+                          <td>admin@gmail.com</td>
+                          <td><span class="tag tag-danger">ADMIN</span></td>
+                          <td>25/12/2018</td>
+                          <td>
+                            <button class="btn btn-outline-primary btn-sm">
+                              <i class="fa fa-edit"></i>
+                            </button>
+                            <button class="btn btn-outline-danger btn-sm ml-1">
+                              <i class="fa fa-trash"></i>
+                            </button>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>1</td>
+                          <td>Admin admin</td>
+                          <td>admin@gmail.com</td>
+                          <td><span class="tag tag-danger">ADMIN</span></td>
+                          <td>25/12/2018</td>
+                          <td>
+                            <button class="btn btn-outline-primary btn-sm">
+                              <i class="fa fa-edit"></i>
+                            </button>
+                            <button class="btn btn-outline-danger btn-sm ml-1">
+                              <i class="fa fa-trash"></i>
+                            </button>
+                          </td>
+                        </tr>
+                        <tr>
+                          <td>1</td>
+                          <td>Admin admin</td>
+                          <td>admin@gmail.com</td>
+                          <td><span class="tag tag-danger">ADMIN</span></td>
+                          <td>25/12/2018</td>
+                          <td>
+                            <button class="btn btn-outline-primary btn-sm">
+                              <i class="fa fa-edit"></i>
+                            </button>
+                            <button class="btn btn-outline-danger btn-sm ml-1">
+                              <i class="fa fa-trash"></i>
+                            </button>
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                  <!-- /.card-body -->
+                     
+    
+                </div>
+                <!-- /.card -->
+              </div>
+            </div>
+            
+        </div>
+    </div>
+</div>
+@endsection
+<style lang="css">
+    .select-numfilas{
+      
+    }
+  </style>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -109,36 +109,6 @@ scratch. This page gets rid of all links and provides the needed markup only.
         </div>
       </li>
       <!-- Notifications Dropdown Menu -->
-      <li class="nav-item dropdown">
-        <a class="nav-link" data-toggle="dropdown" href="#">
-          <i class="far fa-bell"></i>
-          <span class="badge badge-warning navbar-badge">15</span>
-        </a>
-        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-          <span class="dropdown-header">15 Notifications</span>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <i class="fas fa-envelope mr-2"></i> 4 new messages
-            <span class="float-right text-muted text-sm">3 mins</span>
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <i class="fas fa-users mr-2"></i> 8 friend requests
-            <span class="float-right text-muted text-sm">12 hours</span>
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <i class="fas fa-file mr-2"></i> 3 new reports
-            <span class="float-right text-muted text-sm">2 days</span>
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#" role="button"><i
-            class="fas fa-th-large"></i></a>
-      </li>
     </ul>
   </nav>
   <!-- /.navbar -->
@@ -211,83 +181,13 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0 text-dark">Starter Page</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Starter Page</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
+
     <!-- /.content-header -->
 
     <!-- Main content -->
     <div class="content">
       <div class="container-fluid">
-        <div class="row">
-          <div class="col-lg-6">
-            <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up the bulk of the card's
-                  content.
-                </p>
-
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
-              </div>
-            </div>
-
-            <div class="card card-primary card-outline">
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up the bulk of the card's
-                  content.
-                </p>
-                <a href="#" class="card-link">Card link</a>
-                <a href="#" class="card-link">Another link</a>
-              </div>
-            </div><!-- /.card -->
-          </div>
-          <!-- /.col-md-6 -->
-          <div class="col-lg-6">
-            <div class="card">
-              <div class="card-header">
-                <h5 class="m-0">Featured</h5>
-              </div>
-              <div class="card-body">
-                <h6 class="card-title">Special title treatment</h6>
-
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-
-            <div class="card card-primary card-outline">
-              <div class="card-header">
-                <h5 class="m-0">Featured</h5>
-              </div>
-              <div class="card-body">
-                <h6 class="card-title">Special title treatment</h6>
-
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-              </div>
-            </div>
-          </div>
-          <!-- /.col-md-6 -->
-        </div>
+        @yield('content')
         <!-- /.row -->
       </div><!-- /.container-fluid -->
     </div>
